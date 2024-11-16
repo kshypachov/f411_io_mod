25bbd5d0b5aba51bd19c8257625d8d46dc24e1b2
kshypachov <128974084+kshypachov@users.noreply.github.com>
2024-11-16 19:41:42 +0200

    ssdp
    Added support SSDP UPnP protocol for auto discovery device in local network. Works for  Windows

--- Core/app/ssdp_auto_discovery_app/src/ssdp.c
+++ Core/app/ssdp_auto_discovery_app/src/ssdp.c
@@ -15,7 +15,8 @@
 #define ssdp_dev_name  				dev_common_name
 #define ssdp_dev_manufacturer_name	dev_manufacturer_name
 #define ssdp_dev_model_name			dev_model_name
-#define ssdp_web_port				"8080"
+#define ssdp_dev_ver				dev_sw_ver
+#define ssdp_web_port				"40000"
 #define mac_len						6
 
 static const char ssdp_addr[] = "udp://239.255.255.250:1900";
@@ -27,32 +28,34 @@ struct mg_tcpip_if *mif = NULL;
 
 // Статическое описание устройства для XML
 static const char device_description [] =
-    "<?xml version=\"1.0\"?>"
-    "<root xmlns=\"urn:schemas-upnp-org:device-1-0\">"
-    "<specVersion>"
-    "<major>1</major><minor>0</minor>"
-    "</specVersion>"
-    "<device>"
-    "<deviceType>urn:schemas-upnp-org:device:CedarSwitch:1</deviceType>"
-    "<friendlyName>%s</friendlyName>"
-    "<manufacturer>%s</manufacturer>"
-    "<modelName>$s</modelName>"
-    "<modelNumber>1.0</modelNumber>"
-    "<UDN>uuid:%s</UDN>"
-    "<presentationURL>http://%s/index.html</presentationURL>"
-    "</device>"
+    "<?xml version=\"1.0\"?>\n"
+    "<root xmlns=\"urn:schemas-upnp-org:device-1-0\">\n"
+		"<specVersion>\n"
+			"<major>1</major>\n"
+			"<minor>0</minor>\n"
+		"</specVersion>\n"
+		"<device>\n"
+			"<deviceType>urn:schemas-upnp-org:device:Basic:1</deviceType>\n"
+			"<friendlyName>%s</friendlyName>\n"
+			"<manufacturer>%s</manufacturer>\n"
+			"<modelName>%s</modelName>\n"
+			"<modelNumber>%s</modelNumber>\n"
+			"<UDN>UUID:%s</UDN>\n"
+			"<presentationURL>http://%s/index.html</presentationURL>\n"
+		"</device>\n"
     "</root>\r\n";
 
 // SSDP NOTIFY сообщение
 static const char ssdp_notify_template[] =
     "NOTIFY * HTTP/1.1\r\n"
+	"NTS: ssdp:alive\r\n"
     "HOST: 239.255.255.250:1900\r\n"
     "CACHE-CONTROL: max-age=1800\r\n"
+    "SERVER: STM32 Mongoose/%s UPnP/2.0\r\n"
+	"CONFIGID.UPNP.ORG:1\r\n"
     "LOCATION: http://%s:%s/device_description.xml\r\n"
-    "NT: urn:schemas-upnp-org:device:CedarSwitch:1\r\n"
-    "NTS: ssdp:alive\r\n"
-    "SERVER: STM32/Mongoose UPnP/1.1 CedarSwitch/1.0\r\n"
-    "USN: uuid:%s::urn:schemas-upnp-org:device:CedarSwitch:1\r\n"
+    "NT:upnp:rootdevice\r\n"
+    "USN:%s::upnp:rootdevice\r\n"
     "\r\n";
 
 
@@ -63,15 +66,15 @@ static void send_ssdp_notify(void *param) {
 	struct mg_mgr *mgr = (struct mg_mgr *)param;
 	struct mg_connection *udp_conn = mg_connect(mgr, ssdp_addr, NULL, NULL);
 
-	size_t buff_len = (strlen(ssdp_notify_template) + IP_v6_STR_LEN  + strlen(ssdp_web_port) + 20);
+	size_t buff_len = (strlen(ssdp_notify_template) + IP_v6_STR_LEN  + strlen(ssdp_web_port) + 200);
 	buff = calloc(buff_len, sizeof(char));  // +1 для '\0'
 	if (!buff) return; //TODO add log
-	mg_snprintf(ip_addr_str, sizeof(char) * IP_v6_STR_LEN, "%M", mg_print_ip, udp_conn->loc);
+	mg_snprintf(ip_addr_str, sizeof(char) * IP_v6_STR_LEN, "%M", mg_print_ip, &udp_conn->loc);
 
-	snprintf(buff, buff_len, ssdp_notify_template, ip_addr_str, ssdp_web_port, dev_id);
+	snprintf(buff, buff_len-1, ssdp_notify_template, MG_VERSION, ip_addr_str, ssdp_web_port, dev_id);
 
     if (udp_conn != NULL) {
-        mg_send(udp_conn, ssdp_notify_template, strlen(ssdp_notify_template));
+        mg_send(udp_conn, buff, strlen(buff));
         mg_close_conn(udp_conn);
     }
     free(buff);
@@ -97,14 +100,21 @@ void ssdp_set_id(const uint8_t* id, uint8_t id_len){
 
 static void dev_description_handler(struct mg_connection *c, struct mg_http_message *hm){
 	if (mg_match(hm->method, mg_str("GET"), NULL)){
-		mg_http_reply(c, 200, "", device_description, ssdp_dev_name, ssdp_dev_manufacturer_name, ssdp_dev_model_name, dev_id, ip_addr_str);
-
+		mg_http_reply(c, 200, "", device_description, ssdp_dev_name, ssdp_dev_manufacturer_name, ssdp_dev_model_name, ssdp_dev_ver, dev_id, ip_addr_str);
 	}else{
 		mg_http_reply(c, 400, "", //TODO delete for release,
 								"{\"status\":\"error\",\"message\":\"Unsupported method, support only GET and POST methods\"}\r\n");
 	}
 
 }
+
+
+static void dev_404_handler(struct mg_connection *c, struct mg_http_message *hm){
+	mg_http_reply(c, 404, "", //TODO delete for release,
+							"{\"status\":\"error\",\"message\":\"Not found\"}\r\n");
+
+}
+
 
 static void ssdp_web_handler(struct mg_connection *c, int ev, void *ev_data){
 
@@ -113,6 +123,8 @@ static void ssdp_web_handler(struct mg_connection *c, int ev, void *ev_data){
 
 		if (mg_match(hm->uri, mg_str("/device_description.xml"), NULL)) {
 			dev_description_handler(c, hm);
+		}else if (mg_match(hm->uri, mg_str("/*"), NULL)) {
+			dev_404_handler(c, hm);
 		}
 	}
 }
@@ -126,7 +138,7 @@ void ssdp_start_server(void * param_mgr, void * param_mif){
 
 	ssdp_set_id(mif->mac, mac_len);
 
-	mg_timer_add(mgr, 30000, MG_TIMER_REPEAT | MG_TIMER_RUN_NOW, &send_ssdp_notify, mgr);
-	mg_http_listen(mgr, "http://0.0.0.0:8080", ssdp_web_handler, mif);
+	mg_timer_add(mgr, 30000, MG_TIMER_REPEAT | MG_TIMER_RUN_NOW, send_ssdp_notify, mgr);
+	mg_http_listen(mgr, "http://0.0.0.0:40000", ssdp_web_handler, mif);
 }
 
