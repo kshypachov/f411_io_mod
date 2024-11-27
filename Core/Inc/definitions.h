c7da279da0421176b10f9cfb8eacedbfda2a9831
kshypachov <128974084+kshypachov@users.noreply.github.com>
2024-11-27 14:42:53 +0200

    changed version

--- Core/Inc/definitions.h
+++ Core/Inc/definitions.h
@@ -17,7 +17,7 @@
 #define MAX_DOMAIN_NAME_LEN   	128
 
 #define dev_hw_ver				"0.2"
-#define dev_sw_ver				"1.0"
+#define dev_sw_ver				"2.0"
 
 #define FIRMWARE_FILE_NAME		"firmware.bin"
 #define FIRMWARE_FILE_MAX_SIZE  250000
